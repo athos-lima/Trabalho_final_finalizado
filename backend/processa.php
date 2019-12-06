00e9d4a41e5ac00f97e7a033514028b068c30b05
athos-lima <55883609+athos-lima@users.noreply.github.com>
2019-12-06 15:58:31 -0300

    Add files via upload

--- backend/processa.php
+++ backend/processa.php
@@ -1,6 +1,6 @@
 <?php
 
-	include_once("conexao.php");
+	include_once("conexao1.php");
 	
 	//$dados = $_FILES['arquivo'];
 	//var_dump($dados);
@@ -19,33 +19,35 @@
 			if($primeira_linha == false){
 				$id = $linha->getElementsByTagName("Data")->item(0)->nodeValue;
 				echo "ID: $id <br>";
-				
-				$nome = $linha->getElementsByTagName("Data")->item(1)->nodeValue;
+
+				$RA = $linha->getElementsByTagName("Data")->item(1)->nodeValue;
+				echo "RA: $RA <br>";
+
+				$nome = $linha->getElementsByTagName("Data")->item(2)->nodeValue;
 				echo "Nome: $nome <br>";
 				
-				$curso = $linha->getElementsByTagName("Data")->item(2)->nodeValue;
+				$curso = $linha->getElementsByTagName("Data")->item(3)->nodeValue;
 				echo "Curso: $curso <br>";
 				
-				$habilidade1 = $linha->getElementsByTagName("Data")->item(3)->nodeValue;
+				$habilidade1 = $linha->getElementsByTagName("Data")->item(4)->nodeValue;
 				echo "Habilidade1: $habilidade1 <br>";
 				
-				$habilidade2 = $linha->getElementsByTagName("Data")->item(4)->nodeValue;
+				$habilidade2 = $linha->getElementsByTagName("Data")->item(5)->nodeValue;
 				echo "Habilidade2: $habilidade2 <br>";
 				
-				$habilidade3 = $linha->getElementsByTagName("Data")->item(5)->nodeValue;
+				$habilidade3 = $linha->getElementsByTagName("Data")->item(6)->nodeValue;
 				echo "Habilidade3: $habilidade3 <br>";
 				
-				$habilidade4 = $linha->getElementsByTagName("Data")->item(6)->nodeValue;
+				$habilidade4 = $linha->getElementsByTagName("Data")->item(7)->nodeValue;
 				echo "Habilidade4: $habilidade4 <br>";
 				
-				$habilidade5 = $linha->getElementsByTagName("Data")->item(7)->nodeValue;
+				$habilidade5 = $linha->getElementsByTagName("Data")->item(8)->nodeValue;
 				echo "Habilidade5: $habilidade5 <br>";
 				
-
 				echo "<hr>";
 				
 				//Inserir o usuário no BD
-				$result_usuario = "INSERT INTO alunos (id, nome, curso, habilidade1, habilidade2, habilidade3, habilidade4, habilidade5) VALUES ('$id', '$nome', '$curso', '$habilidade1', '$habilidade2', '$habilidade3', '$habilidade4', '$habilidade5')";
+				$result_usuario = "INSERT INTO alunos (id, RA, nome, curso, habilidade1, habilidade2, habilidade3, habilidade4, habilidade5) VALUES ('$id', '$RA', '$nome', '$curso', '$habilidade1', '$habilidade2', '$habilidade3', '$habilidade4', '$habilidade5')";
 				$resultado_usuario = mysqli_query($conn, $result_usuario);
 			}
 			$primeira_linha = false;
